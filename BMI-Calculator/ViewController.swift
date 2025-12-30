635730b907300979d0d05189ef5ea0e03865c7ac
Sabi Dhiman <sabi98@live.co.uk>
2025-12-30 21:42:51 +0000

    Connect UI controls and implement BMI calculation logic
    Added IBOutlets and IBActions for height and weight sliders, labels, and the calculate button in ViewController. Updated Main.storyboard to connect UI elements to code. Implemented logic to update labels as sliders change and to calculate BMI when the button is pressed.

--- BMI-Calculator/ViewController.swift
+++ BMI-Calculator/ViewController.swift
@@ -1,19 +1,43 @@
-//
-//  ViewController.swift
-//  BMI-Calculator
-//
-//  Created by Sabi on 24/12/2025.
-//
 
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var heightLabel: UILabel!
+    @IBOutlet weak var weightLabel: UILabel!
+    
+    
+    @IBOutlet weak var weightSlder: UISlider!
+    @IBOutlet weak var heightSlder: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        updateUI()
     }
+    
+    
+    @IBAction func calculatePressed(_ sender: UIButton) {
+        let height = Double(heightSlder.value)
+        let weight = Double(weightSlder.value)
+        
+        let bmi = weight / (pow(height, 2))
+        print(bmi)
+    }
+    
+    @IBAction func heightSliderChanged(_ sender: UISlider) {
+        let rounded = String(format: "%.2f", sender.value)
+        heightLabel.text = "\(rounded)m"
 
-
+    }
+    
+    @IBAction func weightSliderChanged(_ sender: UISlider) {
+        let wholeNum = Int(ceil(sender.value))
+        weightLabel.text = "\(wholeNum)Kg"
+    }
+    
+    func updateUI() {
+        label.text = "Calculate Your BMI"
+        
+    }
 }
 
